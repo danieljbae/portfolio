871e3f4cf5bdc283f16ca15087e8d60c337d3630
Daniel Bae <danieljbaee@hotmail.com>
2023-08-07 03:12:01 -0700

    added smooth scroll functionality to links on Navigation bar + fixed pop up menu  so that it closes when we click on a section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-// import { Link } from "react-scroll";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
     // Store state of navbar
@@ -45,12 +45,15 @@ const NavBar = () => {
                         key={id}
                         className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
                     >
-                        {link}
+                        {/* Smooth scroll animation on links */}
+                        <Link to={link} smooth duration={500}>
+                            {link}
+                        </Link>
                     </li>
                 ))}
             </ul>
 
-            {/* Menu bar icon */}
+            {/* Menu bar icon (Mobile application) */}
             <div
                 // When menu icon is clicked, toggle nav state to True/False
                 onClick={() => setNav(!nav)}
@@ -68,7 +71,15 @@ const NavBar = () => {
                             key={id}
                             className="px-6 cursor-pointer capitalize py-6 text-4xl"
                         >
-                            {link}
+                            {/* Close menu when we click on link */}
+                            <Link
+                                onClick={() => setNav(!nav)}
+                                to={link}
+                                smooth
+                                duration={500}
+                            >
+                                {link}
+                            </Link>
                         </li>
                     ))}
                 </ul>
