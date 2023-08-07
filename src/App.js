eb4080e67774599650f313887afd2d7d7dca01f8
Daniel Bae <danieljbaee@hotmail.com>
2023-08-06 23:15:20 -0700

    finished home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
-  return ( 
-    <div>
-        <NavBar />
-        <Home />
-    </div>
-    
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<SocialLinks />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FaGitHub, FaLinkedIn } from "react-icons";
+
+const SocialLinks = () => {
+	return <div>SocialLinks</div>;
+};
+
+export default SocialLinks;
