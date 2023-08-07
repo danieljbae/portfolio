cb989a29698d4ea930308663f9c205e67237a2d8
Daniel Bae <danieljbaee@hotmail.com>
2023-08-06 22:20:44 -0700

    finalized NavBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
   return ( 
-    <div><NavBar /></div>
+    <div>
+        <NavBar />
+        <Home />
+    </div>
+    
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import heroImage from '../assets/heroImage.png'
+
+const Home = () => {
+  return (
+    <div>
+        <img src={heroImage} alt='' />
+    </div>
+  )
+}
+
+export default Home
