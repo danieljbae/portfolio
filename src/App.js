30fa5fe316bb30c2d651f335aeffa36b671d3b5b
Daniel Bae <danieljbaee@hotmail.com>
2023-08-09 14:29:46 -0700

    removed contact component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
-import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -15,7 +14,6 @@ function App() {
             <About />
             <Portfolio />
             <Experience />
-            <Contact />
 
         </div>
     );
